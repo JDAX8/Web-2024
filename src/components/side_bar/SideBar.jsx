4c3d3476aac8c92b4769bd4f4e361784e6bdd76b
JDAX8 <juanampudia888@gmail.com>
2024-02-16 21:13:12 -0500

    SideBar funtional finally  :D

--- src/components/side_bar/SideBar.jsx
+++ src/components/side_bar/SideBar.jsx
@@ -5,37 +5,44 @@ import { SmallSideBar } from "../Small_sidebar/S_sideBar";
 import './SideBar.css'
 
 export function SideBar() {
-  const [Open, setOpen] = useState(true);
+  const [open, setOpen] = useState(true);
 
   const toggleOpen = () => {
-    setOpen(!Open); 
+    setOpen(!open);
   };
 
   useEffect(() => {
     const button = document.querySelector(".contractbttn");
-    button.addEventListener("click", toggleOpen); 
-    return () => {
-      button.removeEventListener("click", toggleOpen);
+
+    const handleClick = () => {
+      toggleOpen();
     };
-  }, []); // Ejecuta el efecto una sola vez al montar el componente
+
+    button.addEventListener("click", handleClick);
+
+    return () => {
+      button.removeEventListener("click", handleClick);
+    };
+  }, [open]);
+
   return (
     <div className="B-Sidebar">
       <div className="SideBar">
-        {Open &&( // Si el estado Open es verdadero, renderiza el contenido de la barra lateral grande
+        {open && ( 
           <>
-          <div>
-          <Recommended></Recommended>
-          </div>
+            <div>
+              <Recommended></Recommended>
+            </div>
             <div>
               <SideChannel></SideChannel>
             </div>
           </>
         )}
-        {!Open &&( // Si el estado Open es falso, renderiza el contenido de la barra lateral pequeña
+        {!open && ( 
           <>
-          <div>
-            <SmallSideBar></SmallSideBar>
-          </div>
+            <div>
+              <SmallSideBar></SmallSideBar>
+            </div>
           </>
         )}
       </div>
