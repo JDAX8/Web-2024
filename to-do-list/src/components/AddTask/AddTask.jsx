3f3a37adecba78ed331499f89d0aae57810bec85
JDAX8 <115953228+JDAX8@users.noreply.github.com>
2024-03-06 19:33:39 -0500

    Components

--- to-do-list/src/components/AddTask/AddTask.jsx
+++ to-do-list/src/components/AddTask/AddTask.jsx
@@ -1,8 +1,6 @@
-import { useState } from 'react'
 import './AddTask.css'
 
 export function AddTask() {
-  const [count, setCount] = useState([])
 
   return (
     <>
--- to-do-list/src/components/Body/Body.jsx
+++ to-do-list/src/components/Body/Body.jsx
@@ -1,15 +1,24 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import './Body.css'
 import { AddTask } from "../AddTask/AddTask"
+import { CheckBox } from '../checkbox/checkbox'
+import { TaskContainer } from '../Task/Task'
 
 function Body() {
-  const [tasks, setTasks] = useState([])
+  // const [tasks, setTasks] = useState([])
 
   return (
     <>
     <div className="body">
       <AddTask />
+      <div>
+      <CheckBox />
     </div>
+    <div>
+      <TaskContainer />
+    </div>
+    </div>
+    
     </>
   )
 }
