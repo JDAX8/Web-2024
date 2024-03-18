cb5f69b3955eec06f29022940bf5a58ff29ae902
JDAX8 <juanampudia888@gmail.com>
2024-03-18 18:44:24 -0500

    Feat: CheckBox functional

--- to-do-list/src/components/TaskList/TaskList.jsx
+++ to-do-list/src/components/TaskList/TaskList.jsx
@@ -1,21 +1,44 @@
-import './TaskList.css'
+import './TaskList.css';
+import { useState } from 'react';
 
 export function TaskList({ tasks }) {
+  const [taskStates, setTaskStates] = useState(tasks.map(() => false));
+
+  const handleCheckboxChange = (index) => {
+    setTaskStates((prevStates) => {
+      const newStates = [...prevStates];
+      newStates[index] = !newStates[index];
+      return newStates;
+    });
+  };
+
   return (
     <div>
       <h3>Tasks:</h3>
-      <div className='task-container'>
-      <ul> 
-        {tasks.map((task, index) => (
-          <div key={index} className='task-main-container'>
-            <div className='checkbox-title'>
-              <input className="checkbox" type="checkbox"/>
-              <li className='task-list' key={index}>{task.title}</li>
+      <div className="task-container">
+        <ul>
+          {tasks.map((task, index) => (
+            <div key={index} className="task-main-container">
+              <div className="checkbox-title">
+                <input
+                  className="checkbox"
+                  type="checkbox"
+                  checked={taskStates[index]}
+                  onChange={() => handleCheckboxChange(index)}
+                />
+                <li
+                  className="task-list"
+                  style={{
+                    textDecoration: taskStates[index] ? 'line-through' : 'none',
+                  }}
+                >
+                  {task.title}
+                </li>
+              </div>
+              <button className="delete-button">Delete</button>
             </div>
-              <button className='delete-button'>Delete</button>
-          </div>
-        ))}
-      </ul>
+          ))}
+        </ul>
       </div>
     </div>
   );
